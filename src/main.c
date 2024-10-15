947a9847b865fa41020292ab2151234dd1ff4ec5
crative <hannig.sebi@gmail.com>
2024-10-15 22:20:32 +0200

    line/rect drawing

--- src/main.c
+++ src/main.c
@@ -1,13 +1,29 @@
-#include "stdlib.h"
 #include <raylib.h>
 #include <raymath.h>
+#include <stdio.h>
+
+typedef struct {
+  int width;
+  int height;
+} ScreenInfo;
+
+ScreenInfo scrInfo;
+
+typedef struct {
+  Vector2 pos;
+  Vector2 dir;
+  float planeX;
+  float planeY;
+} Player;
+
+int asd[10] = {1, 2, 3, 1, 3, 2, 1, 2, 1, 2};
 
 void initWindow() {
   int monitor = GetCurrentMonitor();
-  int width = GetMonitorWidth(monitor);
-  int height = GetMonitorHeight(monitor);
+  scrInfo.width = GetMonitorWidth(monitor);
+  scrInfo.height = GetMonitorHeight(monitor);
 
-  InitWindow(width, height, "raycaster");
+  InitWindow(scrInfo.width, scrInfo.height, "raycaster");
 
   ToggleBorderlessWindowed();
 
@@ -22,10 +38,24 @@ int main(void) {
 
     ClearBackground(RAYWHITE);
 
-    for (int i = 0; i < 10; i++) {
-      Color color = {arc4random_uniform(100), arc4random_uniform(100), 100,
-                     100};
-      DrawRectangle(5 * i, 10, 5, 70, color);
+    for (int i = 0; i < (int)(sizeof(asd) / sizeof(int)); i++) {
+      Color color;
+      switch (asd[i]) {
+      case 1:
+        color = RED;
+        break;
+      case 2:
+        color = GREEN;
+        break;
+      case 3:
+        color = YELLOW;
+        break;
+      default:
+        color = PURPLE;
+        break;
+      }
+
+      DrawRectangle(30 * i, 0, 30, GetScreenHeight(), color);
     }
 
     EndDrawing();
